f73a7810717632fb4cf77e6611b7155c5a7fc616
m3tlo <daniil.mikheev228@gmail.com>
2024-04-03 22:07:48 +0300

    change files

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,54 @@
 <template>
-  <AppFileInput
-    hint-text="Загрузите изображение"
-    accept="image/*"
-    @file-selected="handleFileSelected"
-  />
+  <form @submit.prevent="fetchForm">
+    <AppFileInput
+      hint-text="Загрузите изображение"
+      accept="image/*"
+      :input-error="error"
+      error-message="Упс... что-то пошло не так"
+      label="Аватар"
+      @file-selected="handleFileSelected"
+      @clean-status="cleanStatus"
+    />
+  </form>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue'
 import AppFileInput from './components/UI/AppFileInput.vue'
 
+const error = ref<true | false>(false)
+
+const getRandomInt = (max: number) => {
+  return Math.floor(Math.random() * max)
+}
+const setError = (isStatusError: boolean) => {
+  error.value = isStatusError
+}
+
 const handleFileSelected = (file: File) => {
+  const random = getRandomInt(4)
+  console.log(random)
+  if (random === 2 || random === 0) {
+    setError(true)
+  } else setError(false)
+
+  fetchForm(file)
+}
+const cleanStatus = () => {
+  setError(false)
+}
+
+const fetchForm = (file: File) => {
+  //   const response = await fetch("/api/form/post", {
+  //   method: "post",
+  //   headers: {
+  //   'Accept': 'application/json',
+  //    'Content-Type': 'application/json'
+  //  },
+  //  body: JSON.stringify({
+  //      Сюда добавляем наш файл для отправки на сервер
+  //   })
+
   console.log(`Отправка файла: ${file} на сервер...`)
 }
 </script>
--- src/components/UI/AppFileInput.vue
+++ src/components/UI/AppFileInput.vue
@@ -17,8 +17,8 @@
 
       <span class="file-input__changed-text">{{ file ? file.name : 'Файл не выбран' }}</span>
     </div>
-    <p :class="['file-input__hint', error ? 'error' : '']">
-      {{ error ? 'Error message' : hintText }}
+    <p :class="['file-input__hint', inputError ? 'error' : '']">
+      {{ inputError ? errorMessage : hintText }}
     </p>
   </label>
 </template>
@@ -30,23 +30,37 @@ import AppLoader from './AppLoader.vue'
 const props = defineProps({
   accept: {
     type: String,
-    default: ''
+    default: '',
+    required: true
   },
   label: {
     type: String,
-    default: 'Label'
+    default: 'Label',
+    required: true
   },
   hintText: {
     type: String,
-    default: 'Hint text'
+    default: 'Hint text',
+    required: true
+  },
+  errorMessage: {
+    type: String,
+    default: 'Error message',
+    required: true
+  },
+  inputError: {
+    type: Boolean,
+    default: false
   }
 })
-const emit = defineEmits(['file-selected'])
+const emit = defineEmits(['file-selected', 'clean-status'])
 
 const file = ref<File | null>(null)
-const error = ref<true | false>(false)
 const loading = ref<true | false>(false)
 
+const setLoading = (isStatusLoading: boolean) => {
+  loading.value = isStatusLoading
+}
 // функция обработки добавления файла с последующим эмитом в родителя
 const handleFileChange = (event: Event) => {
   const target = event.target as HTMLInputElement
@@ -55,15 +69,14 @@ const handleFileChange = (event: Event) => {
 
   event.target.setAttribute('disabled', 'true')
   button.setAttribute('disabled', 'true')
-  loading.value = true
+  setLoading(true)
   setTimeout(() => {
     if (files && files.length > 0) {
       file.value = files[0]
-      console.log(1, files)
 
       emit('file-selected', file)
     }
-    loading.value = false
+    setLoading(false)
     event.target.removeAttribute('disabled')
     button.removeAttribute('disabled')
   }, 1000)
@@ -72,17 +85,19 @@ const handleFileChange = (event: Event) => {
 // функция редиректа клика с button на инпут
 const redirectClick = (event: Event) => {
   // для списка инпутов можно добавить идентификаторы на кнопку и на инпут и сравнивая идентификаторы редиректить клик
+  // так же обращение к элементам ниже можно изменить на обращение к атрибуту ref, но я решил оставить так
   const input = event.target.previousElementSibling
   event.preventDefault()
   input.click()
 }
 const deleteFile = (event: Event) => {
   event.stopPropagation()
-  console.log(event.target)
   const input = event.target.previousElementSibling
   // опять же логику описываю для одного инпута, для списка инпутов можно работать со вложенным объектом
   file.value = null
   input.files = file.value
+
+  emit('clean-status')
 }
 </script>
 
